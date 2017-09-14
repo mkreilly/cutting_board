524a9e3774d1edc79b73ebf9243bad3a107b8417
fscottfoti <fscottfoti@gmail.com>
2017-09-14 14:34:13 -0700

    initial commit which splits parcels on maz boundaries

---
+++ fetch_buildings.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+import osmnx
+import time
+
+print time.ctime()
+
+gdf = gpd.GeoDataFrame.from_file("california.geojson")
+alameda = gdf[gdf.name == "Alameda County, CA"].iloc[0]
+
+buildings = osmnx.buildings.create_buildings_gdf(alameda.geometry)
+
+print time.ctime()
+
+#open("buildings.geojson", "w").write(buildings.to_json())
+buildings.to_csv("buildings.csv", encoding='utf-8')
+print time.ctime()
