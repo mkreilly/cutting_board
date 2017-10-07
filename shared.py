f34309dd2c510bc24eb1430aeeef0cbb3571ff6b
fscottfoti <fscottfoti@gmail.com>
2017-10-06 20:24:47 -0700

    beginnging reorg from notebook to seprate scripts`

---
+++ shared.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import geopandas as gpd
+
+
+# someday this will be in geopandas
+# setting the crs shouldn't be in here, but all my cases use it
+def read_geocsv(*args, **kwargs):
+    df = pd.read_csv(*args, **kwargs)
+    df["geometry"] = [wkt.loads(s) for s in df["geometry"]]
+    gdf = gpd.GeoDataFrame(df)
+    gdf.crs = {'init': 'epsg:4326'}
+    return gdf
+gpd.read_geocsv = read_geocsv
+
+
+# make a link to look at google maps at a lat-lng
+def feature_to_maps_link(row):
+    centroid = row.centroid
+    return "http://www.google.com/maps/place/%f,%f" % (centroid.y, centroid.x)
+
+
+# geopandas plot two layers in relation to each other
+def two_layer_map(top_layer, bottom_layer, column=None):
+    ax = bottom_layer.plot(figsize=(10, 8), column=column,
+                           legend=(column is not None))
+    return top_layer.plot(ax=ax, color='pink', alpha=0.5, edgecolor="black")
+
+
+# we're in 4326, so we need to convert the crs to meters and return the area
+def compute_area(gdf):
+    gdf.crs = {'init': 'epsg:4326'}
+    return gdf.to_crs(epsg=3395).area
+
+
+# compute the percent area contained in this shape from the shapes in the df
+def compute_pct_area(df, total_area):
+    df["calc_area"] = compute_area(df).values
+    df["pct_area"] = df["calc_area"] / total_area
+    return df
---
+++ split_parcels.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import time
+
+# This script joins parcels mazs and splits them along maz boundaries
+# it reads parcels.csv and mazs.csv and writes split_parcels.csv
+
+print "Loading parcels and mazs"
+parcels = gpd.read_geocsv("parcels.csv")
+mazs = gpd.read_geocsv("mazs.csv")[["maz_id", "geometry"]]
+
+# join mazs to parcels
+print "Joining parcels to mazs"
+joined_parcels = gpd.sjoin(parcels, mazs, how="inner", op='intersects')
+
+
+# when we intersect parcels with mazs, we want to merge parcel slivers back to
+# the main shape - we don't need to keep small slivers of parcels that could be
+# geometric errors
+def merge_slivers_back_to_shapes(shapes, slivers):
+    for label, row in slivers.iterrows():
+
+        distances = [
+            row.geometry.distance(row2.geometry)
+            for _, row2 in shapes.iterrows()
+        ]
+
+        min_ind = np.argmin(distances)
+        closest_shape = shapes.iloc[min_ind]
+        closest_index = shapes.index[min_ind]
+
+        union = closest_shape.geometry.union(row.geometry)
+        shapes = shapes.set_value(closest_index, "geometry", union)
+
+    return shapes
+
+
+# split a parcel by mazs
+def split_parcel(parcel, split_shapes, dont_split_pct_cutoff=.01,
+                 drop_not_in_maz=False):
+    try:
+        overlay = gpd.overlay(parcel, split_shapes.reset_index(),
+                              how='identity')
+    except:
+        # one fails for some reason
+        print "Parcel failed"
+        return
+
+    overlay = compute_pct_area(overlay, compute_area(parcel).sum())
+
+    # now we need to make sure we don't split off very small portions
+    split = overlay[overlay.pct_area >= dont_split_pct_cutoff].copy()
+    dont_split = overlay[overlay.pct_area < dont_split_pct_cutoff]
+
+    split = merge_slivers_back_to_shapes(split, dont_split)
+
+    if drop_not_in_maz:
+        split = split[~split.maz_id.isnull()]
+
+    # have to recompute merge of slivers
+    split = compute_pct_area(split, compute_area(split).sum())
+
+    return split
+
+
+# this does the parcel splits
+print time.ctime()
+
+cnt = 0
+split_parcels = []
+apn_counts = joined_parcels.index.value_counts()
+bad_apns = ["999 999999999"]
+
+print "Splitting parcels when there are overlaps"
+for apn, count in apn_counts.iteritems():
+
+    if apn in bad_apns:
+        continue
+
+    if count < 2:
+        # not an overlap, doesn't need to be split
+        continue
+
+    subset = joined_parcels.loc[apn]
+
+    ret = split_parcel(subset.head(1).drop("maz_id", axis=1),
+                       mazs[mazs.index.isin(subset.maz_id)],
+                       drop_not_in_maz=True, dont_split_pct_cutoff=.03)
+
+    if ret is None:
+        continue
+
+    ret["orig_apn"] = apn
+    # make a new unique apn when we split a parcel
+    ret["apn"] = [apn + "-" + str(i+1) for i in range(len(ret))]
+    new_parcels.append(ret)
+
+    cnt += 1
+    if cnt % 100 == 0:
+        print "Done %d of %d" % (cnt, len(apn_counts))
+
+split_parcels = pd.concat(split_parcels)
+
+print time.ctime()
+
+
+joined_parcels["orig_apn"] = joined_parcels.index
+# this is a little hard to read, but the idea is that we want to take all the
+# parcels and their associated maz_ids which were NOT overlaps and merge them
+# with all the splits of the overlapping parcels that we just did
+# after this line we should have all the parcels
+split_parcels = gpd.GeoDataFrame(
+    pd.concat([
+        joined_parcels[~joined_parcels.index.isin(split_parcels.orig_apn)],
+        split_parcels
+    ])
+)
+
+split_parcels.to_csv("split_parcels.csv")
